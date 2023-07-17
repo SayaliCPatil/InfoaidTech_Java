b4885c0cc74ef59e006f48965126948bc368ecd8
SayaliCPatil <sayalicpatil21@gmail.com>
2023-07-18 00:00:44 +0530

    Password Generator using user set of characters in java

--- PassWordGenerator.java
+++ PassWordGenerator.java
@@ -1,24 +1,38 @@
+package pratice;
 import java.util.*;
-import java.util.stream.Collectors;  
-class PassWordGenerator{
+import java.util.stream.Collectors;
+
+public class PassWordGenerator {
+
     public static void main(String[] args) {
-        Scanner Input =  new Scanner(System.in);
-        System.out.println("Enter the length of the password :");
-        int plen = Input.nextInt();
-        System.out.println("Enter the charachter set to be used to generat the password :");
-        String chSet = Input.next();
+        Scanner input =  new Scanner(System.in);
+        boolean generateMore = true;
 
-        List<Character> listOfChar = chSet.chars()  
-        .mapToObj(data -> (char) data)  
-        .collect(Collectors.toList());  
+        while (generateMore) {
+            System.out.println("Enter the length of the password:");
+            int plen = input.nextInt();
+            System.out.println("Enter the character set to be used to generate the password:");
+            String chSet = input.next();
 
-        Collections.shuffle(listOfChar); 
+            List<Character> listOfChar = chSet.chars()
+                    .mapToObj(data -> (char) data)
+                    .collect(Collectors.toList());
 
-        String password = listOfChar.stream()  
-                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)  
-                .toString();
+            Collections.shuffle(listOfChar);
 
-        System.out.println("The password is : " +password);
+            StringBuilder passwordBuilder = new StringBuilder(plen);
+            for (int i = 0; i < plen; i++) {
+                int randomIndex = (int) (Math.random() * listOfChar.size());
+                passwordBuilder.append(listOfChar.get(randomIndex));
+            }
 
+            String password = passwordBuilder.toString();
+            System.out.println("The password is: " + password);
+
+            System.out.println("Do you want to regenerate the password? (Y/N):");
+            String regenerateChoice = input.next();
+            generateMore = regenerateChoice.equalsIgnoreCase("Y");
+        }
+        System.out.println("Thank you for using the Password Generator!");
     }
 }
